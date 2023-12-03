9cf247eb370c350ab6f4d3c363f42f27d0906e8f
NathanaelEastwood <118187803+NathanaelEastwood@users.noreply.github.com>
2023-12-03 12:12:17 +0000

    Initial Commit.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.text-container').addClass('scroll-up-animation');
+    }, 500); // Set the delay in milliseconds (2000ms = 2 seconds in this example)
+});
